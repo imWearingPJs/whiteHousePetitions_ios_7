95f19a49a23c5dd9434c91dd06626fadefae52cb
Michael Kozub <michaelkozub@gmail.com>
2018-11-13 17:21:12 -0700

    Initial Commit

---
+++ whiteHousePetitions_ios_7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whiteHousePetitions_ios_7
+//
+//  Created by Michael Kozub on 11/13/18.
+//  Copyright © 2018 Michael Kozub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
