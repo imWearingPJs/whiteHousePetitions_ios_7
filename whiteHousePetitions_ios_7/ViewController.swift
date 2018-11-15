c531edbaa632e823c523e9a145ea313e48fb7cfe
Michael Kozub <michaelkozub@gmail.com>
2018-11-15 08:37:07 -0700

    started a clean project

--- whiteHousePetitions_ios_7/ViewController.swift
+++ whiteHousePetitions_ios_7/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  whiteHousePetitions_ios_7
 //
-//  Created by Michael Kozub on 11/13/18.
+//  Created by Michael Kozub on 11/15/18.
 //  Copyright © 2018 Michael Kozub. All rights reserved.
 //
 
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
